3d2078b7a333fda3fcda044a6ac18cde882747e2
Sorcen <serferjim1598753@gmail.com>
2019-04-27 16:20:12 +0200

    Implementadas las nuevas queries para la gestion de torneos

--- src/main/java/com/ispp/thorneo/repository/PunctuationRepository.java
+++ src/main/java/com/ispp/thorneo/repository/PunctuationRepository.java
@@ -1,5 +1,7 @@
 package com.ispp.thorneo.repository;
 
+import java.util.List;
+
 import com.ispp.thorneo.domain.Punctuation;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
@@ -12,4 +14,12 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PunctuationRepository extends JpaRepository<Punctuation, Long> {
 
+    @Query("select p from Participation p where p.tournament.id = ?1")
+    List<Punctuation> getPunctuationsByTournament(Long tournamentId);
+
+    @Query("select max(p.round) from Punctuation p where p.tournament.id = ?1")
+    Integer getMaxRoundTournament(Long tournamentId);
+
+    @Query("select p from Punctuation p where p.round = ?1 and p.tournament.id = ?2")
+    List<Punctuation> getPuntuationsByRoundAndTournament(Integer round, Long tournamentId);
 }
