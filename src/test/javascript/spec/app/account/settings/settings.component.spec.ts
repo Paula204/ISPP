77fefb22456d206df5126c56dca58043f8b3a01b
migcarde <mcaraballodelarosa@outlook.com>
2019-05-01 16:02:32 +0200

    fix

--- src/test/javascript/spec/app/account/settings/settings.component.spec.ts
+++ src/test/javascript/spec/app/account/settings/settings.component.spec.ts
@@ -13,7 +13,7 @@ describe('Component Tests', () => {
 
         beforeEach(async(() => {
             TestBed.configureTestingModule({
-                imports: [ThorneoTestModule],
+                imports: [ThorneoTestModule, AccountService],
                 declarations: [SettingsComponent],
                 providers: []
             })
