57ee3fe7fbe1990f13f39e2d14ef3d8ec936e0b2
migcarde <mcaraballodelarosa@outlook.com>
2019-05-01 11:51:59 +0200

    fix

--- src/main/webapp/app/account/account.module.ts
+++ src/main/webapp/app/account/account.module.ts
@@ -14,6 +14,7 @@ import {
     SettingsDeleteDialogComponent,
     accountState
 } from './';
+import { AccountService } from 'app/core';
 
 @NgModule({
     imports: [ThorneoSharedModule, RouterModule.forChild(accountState)],
@@ -28,6 +29,7 @@ import {
         SettingsDeleteDialogComponent
     ],
     entryComponents: [SettingsDeleteDialogComponent],
+    providers: [AccountService],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class ThorneoAccountModule {}
