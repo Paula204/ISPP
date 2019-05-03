6cfb6e925ccfafc2bd7f08bbe3761d9cdb672b06
Jhosue Ider <jhosueyder@gmail.com>
2019-05-03 17:16:15 +0200

    Agregado widget de twitter e instagram

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { JhiEventManager } from 'ng-jhipster';
 
